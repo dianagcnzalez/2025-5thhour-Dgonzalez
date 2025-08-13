366f5ba91e391d6331a8817daabd8c06b57a08a4
Diana Gonzalez <dg126055@student.wpsok.org>
2025-08-13 13:09:02 -0500

    added Hw1

---
+++ HW2.py
@@ -0,0 +1,28 @@
+#Name: Diana Gonzalez
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print ("Hello World!")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+anything = 3
+anything2 ="OneHappyCouple"
+anything3 = True
+#3. Print all three variables on the same print function (at the same time).
+print (anything, anything2, anything3)
+#4. Create a variable that asks the user to input an integer.
+x = int(input("Enter a number: "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+y= anything + x
+print (y)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print (y/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+anything3 = False
+#8. Print the value of the boolean variable.
+print (anything3)
+#9. Create a variable with a number that contains decimals.
+mardi = 5.4
+#10. Round the number from #9 up or down using the round function.
+print(round(mardi))
